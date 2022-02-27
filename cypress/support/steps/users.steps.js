0533058eb04d98d72fc20069bcf921946db802ab
raphaelpalhano <raphael1angel@hotmail.com>
2022-02-27 17:13:13 -0300

    eslintrc.json: quotes using single quote and backtick

--- cypress/support/steps/users.steps.js
+++ cypress/support/steps/users.steps.js
@@ -2,13 +2,13 @@
 import { When, Then, Given, And } from 'cypress-cucumber-preprocessor/steps';
 
 
-Given('that register a user type {string}', (register_type) => {
+Given(`that register a user type {string}`, (register_type) => {
     cy.postUserByType(register_type).then((res) => {
     
     });
 });
 
-Given('that is logged with {string}', (login_type) => {
+Given(`that is logged with {string}`, (login_type) => {
     cy.loginWith(login_type).then((login_response) => {
         expect(login_response.body).to.have.property('message');
         expect(login_response.body.message).to.equal('Login realizado com sucesso');
@@ -49,7 +49,7 @@ When('request all the users from /usuarios', () => {
     });
 });
 
-Given('post the user of type {string}', (user_type) => {
+Given(`post the user of type {string}`, (user_type) => {
     cy.postUserByType(user_type).then((post_response) => {
         cy.wrap(post_response).as('Response');
     });
